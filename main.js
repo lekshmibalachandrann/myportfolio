eb120514248b971c55b0e79600b00434490be8a4
lekshmibalachandrann <lekshmibalachandran501@gmail.com>
2025-09-28 12:11:13 +0530

    modifications

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings:["Frontend Developer", "Web Developer", "Computer Engineer"],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true
+} )
